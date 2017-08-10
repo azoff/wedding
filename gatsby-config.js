d6a51139d30e18d503330321625727efd8a5a96c
Jonathan Azoff <jon@azof.fr>
2017-08-10 09:43:05 -0700

    import json wip

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,17 @@
 module.exports = {
-	siteMetadata: {
-		title: "Azoff Thayer Wedding",
-	},
-	plugins: ["gatsby-plugin-sass", "gatsby-plugin-react-helmet"],
+  siteMetadata: {
+    title: "Azoff Thayer Wedding",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-react-helmet",
+    "gatsby-transformer-json",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "content",
+        path: `${__dirname}/content/`,
+      },
+    },
+  ],
 }
